f964c3cbb7c1e9a9734c129da21acb0fc1a95886
Roope Karppinen <roope.karppinen@aalto.fi>
2024-09-24 16:32:28 +0300

    Changed style and added wordlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import { useState } from "react"
-
+import list from "./words.json"
 function App() {
-  const [Guess, Set] = useState("Test")
-  return <h1> Hi</h1>
-}
+  const [Guess, Set] = useState(() => {
+    return list[Math.floor(Math.random() * list.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
+  console.log(Guess)
+  return (
+  <div 
+    style= {{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "0 auto",
+      alignItems: "center",
+  }}
+  >
+    <div style={{ fontSize: "2rem", textAlign: "center"}}>Lose Win</div>
+  </div>
+)
+}
 export default App
