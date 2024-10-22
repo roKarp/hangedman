c6d6b11f0d62bdb06e8b1b3138a927a781f2cd12
Roope Karppinen <roope.karppinen@aalto.fi>
2024-10-22 15:13:51 +0300

    added word logic

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,3 +1,30 @@
 export function HangmanWord() {
-    
+    const word = "init"
+    const guesses = ["i", "h", "n"]
+    return (
+        <div 
+            style={{ 
+                display: "flex", 
+                gap: ".25em", 
+                fontSize: "6rem", 
+                fontWeight: "bold", 
+                textTransform: "uppercase", 
+                fontFamily: "monospace"
+            }}
+        > 
+            {word.split("").map((letter, index) => (
+            <span style={{ borderBottom: ".1em solid black"}} key={index}>
+                <span style={{
+                    visibility: guesses.includes(letter) 
+                    ? "visible"
+                    : "hidden",
+                }}
+            > 
+
+                {letter}
+                </span>
+            </span>
+            ))}
+        </div>
+    )
 }
