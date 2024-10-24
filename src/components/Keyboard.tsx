96436fa492151d95e6febe99e8b7965b35f82545
Roope Karppinen <roope.karppinen@aalto.fi>
2024-10-24 14:54:16 +0300

    created the base keyboard grid

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,3 +1,21 @@
+const alph = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z'
+];
 export function Keyboard() {
-    
+    return (
+        <div 
+            style={{ 
+            display: 'grid', 
+            gridTemplateColumns: 'repeat(auto-fit, minmax(75px, 1fr))', 
+            gap:'.5rem',
+        }}>
+            {alph.map(key => {
+                return (
+                    <button key={key} >{key}</button>
+                )
+            })}
+        </div>
+    )
 }
